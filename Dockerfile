7c02c270e140bb6985f394c972ed6460ab80c467
snowdream <sn0wdr1am@qq.com>
2024-07-20 21:22:33 +0800

    feat: openjdk 11.0.24

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@ FROM snowdreamtech/alpine:3.20.0
 
 LABEL maintainer="snowdream <sn0wdr1am@qq.com>"
 
-ENV JAVA_VERSION=8.402.06-r0 \
+ENV JAVA_VERSION=11.0.24_p8-r0 \
     JAVA_HOME=/usr/lib/jvm/default-jvm \
     PATH=${PATH}:${JAVA_HOME}/bin:${JAVA_HOME}/jre/bin
 
-RUN apk add --no-cache openjdk8=${JAVA_VERSION}
+RUN apk add --no-cache openjdk11=${JAVA_VERSION}
--- build.sh
+++ build.sh
@@ -7,20 +7,20 @@ GITHUB_PROJECT1=ghcr.io/snowdreamtech/openjdk
 
 docker buildx build --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le,linux/riscv64,linux/s390x \
     -t ${DOCKER_HUB_PROJECT}:latest \
-    -t ${DOCKER_HUB_PROJECT}:8.402.06 \
-    -t ${DOCKER_HUB_PROJECT}:8.402 \
-    -t ${DOCKER_HUB_PROJECT}:8 \
+    -t ${DOCKER_HUB_PROJECT}:11.0.24 \
+    -t ${DOCKER_HUB_PROJECT}:11.0 \
+    -t ${DOCKER_HUB_PROJECT}:11 \
     -t ${GITHUB_PROJECT}:latest \
-    -t ${GITHUB_PROJECT}:8.402.06 \
-    -t ${GITHUB_PROJECT}:8.402 \
-    -t ${GITHUB_PROJECT}:8 \
+    -t ${GITHUB_PROJECT}:11.0.24 \
+    -t ${GITHUB_PROJECT}:11.0 \
+    -t ${GITHUB_PROJECT}:11 \
     -t ${DOCKER_HUB_PROJECT1}:latest \
-    -t ${DOCKER_HUB_PROJECT1}:8.402.06 \
-    -t ${DOCKER_HUB_PROJECT1}:8.402 \
-    -t ${DOCKER_HUB_PROJECT1}:8 \
+    -t ${DOCKER_HUB_PROJECT1}:11.0.24 \
+    -t ${DOCKER_HUB_PROJECT1}:11.0 \
+    -t ${DOCKER_HUB_PROJECT1}:11 \
     -t ${GITHUB_PROJECT1}:latest \
-    -t ${GITHUB_PROJECT1}:8.402.06 \
-    -t ${GITHUB_PROJECT1}:8.402 \
-    -t ${GITHUB_PROJECT1}:8 \
+    -t ${GITHUB_PROJECT1}:11.0.24 \
+    -t ${GITHUB_PROJECT1}:11.0 \
+    -t ${GITHUB_PROJECT1}:11 \
     . \
     --push
