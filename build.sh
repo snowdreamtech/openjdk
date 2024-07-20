e809669237dde32c4b67ba9bac213c47b86c79e9
snowdream <sn0wdr1am@qq.com>
2024-07-20 21:32:42 +0800

    feat: openjdk 21.0.4

--- build.sh
+++ build.sh
@@ -7,20 +7,20 @@ GITHUB_PROJECT1=ghcr.io/snowdreamtech/openjdk
 
 docker buildx build --platform=linux/amd64,linux/arm64,linux/ppc64le,linux/s390x \
     -t ${DOCKER_HUB_PROJECT}:latest \
-    -t ${DOCKER_HUB_PROJECT}:17.0.12 \
-    -t ${DOCKER_HUB_PROJECT}:17.0 \
-    -t ${DOCKER_HUB_PROJECT}:17 \
+    -t ${DOCKER_HUB_PROJECT}:21.0.4 \
+    -t ${DOCKER_HUB_PROJECT}:21.0 \
+    -t ${DOCKER_HUB_PROJECT}:21 \
     -t ${GITHUB_PROJECT}:latest \
-    -t ${GITHUB_PROJECT}:17.0.12 \
-    -t ${GITHUB_PROJECT}:17.0 \
-    -t ${GITHUB_PROJECT}:17 \
+    -t ${GITHUB_PROJECT}:21.0.4 \
+    -t ${GITHUB_PROJECT}:21.0 \
+    -t ${GITHUB_PROJECT}:21 \
     -t ${DOCKER_HUB_PROJECT1}:latest \
-    -t ${DOCKER_HUB_PROJECT1}:17.0.12 \
-    -t ${DOCKER_HUB_PROJECT1}:17.0 \
-    -t ${DOCKER_HUB_PROJECT1}:17 \
+    -t ${DOCKER_HUB_PROJECT1}:21.0.4 \
+    -t ${DOCKER_HUB_PROJECT1}:21.0 \
+    -t ${DOCKER_HUB_PROJECT1}:21 \
     -t ${GITHUB_PROJECT1}:latest \
-    -t ${GITHUB_PROJECT1}:17.0.12 \
-    -t ${GITHUB_PROJECT1}:17.0 \
-    -t ${GITHUB_PROJECT1}:17 \
+    -t ${GITHUB_PROJECT1}:21.0.4 \
+    -t ${GITHUB_PROJECT1}:21.0 \
+    -t ${GITHUB_PROJECT1}:21 \
     . \
     --push
